0c13488aca355c44f89151991bd7736453424540
Nelson Silva <nsilva19@alumnos.utalca.cl>
2022-09-04 17:00:27 -0400

    Revert "Merge branch 'HEAD' into prueba"
    This reverts commit 32ded44bc63d7b767be1cdf7c460b606cc4000b0.

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module codigo.proyectoprogramacion {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens codigo.proyectoprogramacion to javafx.fxml;
-    exports codigo.proyectoprogramacion;
-}
