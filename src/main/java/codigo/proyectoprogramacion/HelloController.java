b3ea6e5d5ce220e011bb128cb2a1ef01cbe35ea0
Nelson Silva <nsilva19@alumnos.utalca.cl>
2022-09-04 16:58:44 -0400

    Actualización proyecto

---
+++ src/main/java/codigo/proyectoprogramacion/HelloController.java
@@ -0,0 +1,14 @@
+package codigo.proyectoprogramacion;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
