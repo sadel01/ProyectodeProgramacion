50eb7a3561a181d3c38431915098a096fec515d4
Nelson Silva <nsilva19@alumnos.utalca.cl>
2022-09-18 16:38:45 -0300

    actualizacion panel coordenadas

--- src/main/java/codigo/proyecto/Ventana.java
+++ src/main/java/codigo/proyecto/Ventana.java
@@ -11,11 +11,11 @@ public class Ventana extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Ventana.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1600, 900);
+        Scene scene = new Scene(fxmlLoader.load(), 1600, 800);
         stage.setTitle("Transformador a Script");
         stage.setScene(scene);
         stage.setMinWidth(1600);
-        stage.setMinHeight(940);
+        stage.setMinHeight(840);
         stage.show();
     }
 
