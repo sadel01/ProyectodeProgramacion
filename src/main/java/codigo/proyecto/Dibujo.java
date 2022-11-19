50c534b182231d86c156aebb050fefc911d7e642
Rodrigo Dominguez Araya <rdominguez19@alumnos.utalca.cl>
2022-11-19 01:58:57 -0300

    tamaño 'a'

--- src/main/java/codigo/proyecto/Dibujo.java
+++ src/main/java/codigo/proyecto/Dibujo.java
@@ -107,10 +107,10 @@ public class Dibujo {
                 l1.setStrokeWidth(grosor);
                 root.getChildren().add(l1);
             }
-            x = 30;
-            y = y + 150;
+            x = 30*tamanio;
+            y = y + 150*tamanio;
             xInicialSu = x;
-            yInicialSu = y + 55;
+            yInicialSu = y + 55*tamanio;
         }
         if (caracter == ' ') {
             if (x != 30) {
@@ -136,7 +136,7 @@ public class Dibujo {
             
             if (caracter == 'a' || caracter == 'á') {
 
-                Text t = new Text("\n"+caracter+":");
+               /* Text t = new Text("\n"+caracter+":");
                 textoCoord.getChildren().add(t);
                 pts(textoCoord, root, puntosDeControl, x+30, y+10, x+25, y+30, x-5, y-30, x-20, y+85);
                 pts(textoCoord, root, puntosDeControl, x+35, y, x+60, y+15, x+10, y+60, x+50, y+65);
@@ -177,10 +177,55 @@ public class Dibujo {
                     }
                     cont++;
                 }
-
-                x = x + 60;
+                x=x+60;
                 if (auxSub) {
                     Subrayar(xInicialSu, yInicialSu*tamanio, x*tamanio, root);
+                }*/
+
+                Text t = new Text("\n"+caracter+":");
+                textoCoord.getChildren().add(t);
+                pts(textoCoord, root, puntosDeControl, x+30, y+10, x+25, y+30, x-5, y-30, x-20, y+85);
+                pts(textoCoord, root, puntosDeControl, x+35, y, x+60, y+15, x+10, y+60, x+50, y+65);
+
+                fun(root, puntosDeControl, x + 30, y + 10, x - 5, y - 30, x - 20, y + 85, x + 25, y + 30, x + 35, y, x + 10, y + 60, x + 50, y + 65, x + 60, y + 15);
+
+                while(cont < auxBold){
+                    CubicCurve c = new CubicCurve(x + 30*tamanio, y + 10*tamanio, x - 5*tamanio, y - 30*tamanio, x- 20*tamanio, y + 85*tamanio, x + 25*tamanio, y + 30*tamanio);
+                    c.setFill(Color.TRANSPARENT);
+                    c.setStroke(color);
+                    c.setStrokeWidth(grosor);
+
+
+                    CubicCurve c2 = new CubicCurve(x+ 35*tamanio, y, x + 10*tamanio, y + 60*tamanio, x + 50*tamanio, y + 65*tamanio, x + 60*tamanio, y + 15*tamanio);
+                    c2.setFill(Color.TRANSPARENT);
+                    c2.setStroke(color);
+                    c2.setStrokeWidth(grosor);
+
+                    root.getChildren().add(c);
+                    root.getChildren().add(c2);
+
+                    if (caracter == 'á') {
+                        Line tilde = new Line((x + 15)*tamanio, (y - 10)*tamanio, (x + 30)*tamanio, (y - 30)*tamanio);
+                        tilde.setFill(Color.TRANSPARENT);
+                        tilde.setStroke(color);
+                        tilde.setStrokeWidth(grosor);
+
+                        if(cont < 1){
+                            pts(textoCoord, root, puntosDeControl, x+15, y-10, x+30, y-30);
+                            fun(root, puntosDeControl, x + 15, y - 10, x + 30, y - 30);
+                        }
+
+                        root.getChildren().add(tilde);
+                    }
+
+                    if(auxBold > 1){
+                        x++;
+                    }
+                    cont++;
+                }
+                x=x+60*tamanio;
+                if (auxSub) {
+                    Subrayar(xInicialSu, yInicialSu, x, root);
                 }
 
 
@@ -2025,7 +2070,7 @@ public class Dibujo {
                     Subrayar(xInicialSu, yInicialSu*tamanio, x*tamanio, root);
                 }
             } else {
-                if(caracterAnt=='^'){
+                if(caracterAnt=='^' || caracterAnt=='+'){
 
                 }
                 else {
@@ -2784,7 +2829,7 @@ public class Dibujo {
         }
         if (caracter == ' ') {
             if (x != 30) {
-                x = x + 50;
+                x = x + 50*tamanio;
             }
             auxSub = false;
             auxBold = 1;
